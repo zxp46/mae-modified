188435c103257c6818b6c63852975990030dd15b
Zixuan Pan <91744669+zxp46@users.noreply.github.com>
2022-02-14 16:50:30 -0500

    MAE original version added

---
+++ mae-main-original/run_train.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+IMAGENET_DIR="~/data/imagenet"
+PRETRAIN_CHKPT="./exp/checkpoint-306.pth"
+
+python main_finetune.py \
+    --accum_iter 4 \
+    --batch_size 8 \
+    --model vit_large_patch16 \
+    --finetune ${PRETRAIN_CHKPT} \
+    --epochs 100 \
+    --blr 5e-4 --layer_decay 0.65 \
+    --weight_decay 0.05 --drop_path 0.1 --mixup 0.8 --cutmix 1.0 --reprob 0.25 \
+    --dist_eval --data_path ${IMAGENET_DIR} \
+    --output_dir ./exp_train \
+    --resume ./exp_train/checkpoint-72.pth \
+    --log_dir ./exp_train > out_train.txt
