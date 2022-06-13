839584f001a89b721b076dedba6ca44726efe906
Zixuan Pan <91744669+zxp46@users.noreply.github.com>
2022-06-13 14:35:24 -0400

    Finetune and linprob updated

--- distributed_finetune.sh
+++ distributed_finetune.sh
@@ -1,11 +1,15 @@
-PRETRAIN_CHKPT=./exp/c
+#!/bin/bash
+IMAGENET_DIR="~/autodl-tmp/imagenet"
+PRETRAIN_CHKPT="./exp/checkpoint-590.pth"
 
 OMP_NUM_THREADS=1 python -m torch.distributed.launch --nproc_per_node=4 distributed_finetune.py \
     --accum_iter 4 \
     --batch_size 64 \
     --model vit_base_patch16 \
-    --finetune ${PRETRAIN_CHKPT} \
-    --epochs 100 \
+    --finetune ${PRETRAIN_CHKPT}\
+    --epochs 110 --resume ./exp_train/checkpoint-99.pth \
     --blr 5e-4 --layer_decay 0.65 \
     --weight_decay 0.05 --drop_path 0.1 --mixup 0.8 --cutmix 1.0 --reprob 0.25 \
-    --dist_eval --data_path ${IMAGENET_DIR}
+    --dist_eval --data_path ${IMAGENET_DIR} \
+    --output_dir ./exp_train \
+    --log_dir ./exp_train > out_train.txt
